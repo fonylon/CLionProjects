4befe4788d37bb9c9b4be45252855101c9e954bc
fonylon <fonylon@qq.com>
2017-09-14 10:49:13 +0800

    delete all

---
+++ fonylon/aaa.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 风林 on 2017/9/14.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "aaa"<< endl;
+}
---
+++ fonylon/day01/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 风林 on 2017/9/14.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "aaa"<< endl;
+}
